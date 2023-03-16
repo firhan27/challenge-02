bf81db2db43f809a2f7856018293c68e5d3e44b8
firhan27 <muhammadfirhan500@gmail.com>
2023-03-16 22:18:13 +0700

    no1-changed

--- challenge2/no1.js
+++ challenge2/no1.js
@@ -3,11 +3,12 @@
 // Function changeWord menerima tiga parameter: selectedText, changedText, dan text.
 function changeWord(selectedText, changedText, text) {
   /*
-  Function ini menggunakan RegExp constructor untuk membuat regular expression baru dengan opsi "gi" (g = global dan i = case-insensitive).
-  Global akan mencari seluruh kemunculan selectedText pada text. Case-insensitive akan mencocokkan teks tanpa
-  memperdulikan huruf besar atau kecil. 
+  Function ini menggunakan const regex yang memiliki parameter selectedText.
+  Setelah itu akan ada sebuah kondisi yang akan me-return text.replace yang memiliki dua parameter yaitu regex(selectedText) dan changedText.
+  Yang berfungsi untuk mengganti kalimat yang telah di selected(SelectedText) dan akan diganti dengan changedTexted.
+  Lalu akan menampilkan parameter text setelah nilai parameter sudah diubah.
   */
-  const regex = new RegExp(selectedText, "gi");
+  const regex = new RegExp(selectedText);
   return text.replace(regex, changedText);
 }
 
